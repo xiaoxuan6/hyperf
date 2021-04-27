05f1d8ec09dbeeb7072000cf3a15d683b046e076
xiaoxuan6 <1527736751@qq.com>
2021-04-27 19:05:19 +0800

    aop之注解解析

--- app/Aspect/IndexAspect.php
+++ app/Aspect/IndexAspect.php
@@ -8,6 +8,7 @@
 
 namespace App\Aspect;
 
+use App\Annotations\UserAnnotation;
 use App\Controller\AspectController;
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
@@ -22,6 +23,10 @@ class IndexAspect extends AbstractAspect
         AspectController::class
     ];
 
+    public $annotations = [
+        UserAnnotation::class
+    ];
+
     /**
      * @return mixed return the value from process method of ProceedingJoinPoint, or the value that you handled
      */
@@ -30,13 +35,37 @@ class IndexAspect extends AbstractAspect
         // TODO: Implement process() method.
         $result = $proceedingJoinPoint->process();
 
-        // 获取闭包所在的对象
-        $method = $proceedingJoinPoint->originalMethod;
-//        $object = (new \ReflectionFunction($method))->getClosure();
-        $object = (new \ReflectionFunction($method))->getClosureThis();
-        $name = $object->name;
+        // 获取当前类名
+//        $class = $proceedingJoinPoint->className;
+//        var_dump($class);
+//
+        // 获取 class 类所在的 object
+//        $annotation = $proceedingJoinPoint->getReflectMethod();
+//        $class = $annotation->class;
+//        $name = $annotation->name;
+//        var_dump($class, $name);
+//        var_dump($annotation);
 
-//        $proceedingJoinPoint->className;
-        return __CLASS__ . ":" . $result . "_" . $name;
+        // 获取闭包所在的对象
+//        $method = $proceedingJoinPoint->originalMethod;
+//        var_dump($method);
+//        $object = (new \ReflectionFunction($method))->getClosure();
+//        $object = (new \ReflectionFunction($method))->getClosureThis();
+
+        // 获取 class 中的属性 name
+//        $object = $proceedingJoinPoint->getInstance();
+//        $name = $object->name;
+//        var_dump($object);
+
+        // 获取方法中的参数
+//        $arg = $proceedingJoinPoint->getArguments();
+//        var_dump($arg);
+
+        // 获取类中的注解
+//        $data = $proceedingJoinPoint->getAnnotationMetadata()->class;
+//        $annotation = $data[UserAnnotation::class];
+//        var_dump($annotation->name);
+
+        return __CLASS__ . ":" . $result;
     }
 }
--- app/Controller/AspectController.php
+++ app/Controller/AspectController.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Annotations\UserAnnotation;
 use Hyperf\HttpServer\Annotation\AutoController;
 
 /**
  * @AutoController()
+ * @UserAnnotation(age="10",name="vinshon")
  */
 class AspectController
 {
@@ -16,5 +18,10 @@ class AspectController
     public function index()
     {
         return 1;
+    }
+
+    public function get($name = "sdfsd")
+    {
+        return __METHOD__ . $name;
     }
 }
