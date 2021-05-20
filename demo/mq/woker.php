722cb82a0ca988863e162ac27209e60101d8cdc4
xiaoxuan6 <1527736751@qq.com>
2021-05-20 18:06:27 +0800

    mq 消息确认

--- demo/mq/woker.php
+++ demo/mq/woker.php
@@ -32,6 +32,8 @@ $callabck = function ($msg) {
     echo " [x] Received ", $msg->body, "\n";
     sleep(substr_count($msg->body, '.') ?? 2);
     echo " [x] Done", "\n";
+    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+    // 一个很容易犯的错误就是忘了basic_ack，后果很严重。消息在你的程序退出之后就会重新发送，如果它不能够释放没响应的消息，RabbitMQ就会占用越来越多的内存。
 };
 
 #翻译时注：只有consumer已经处理并确认了上一条message时queue才分派新的message给它
@@ -43,7 +45,10 @@ $channel->basic_qos(null, 1, null);
  * global：上述限制的限定对象，false=限制单个消费者；true=限制整个信道
  */
 
-$channel->basic_consume($queueName, "", false, true, false, false, $callabck);
+//$channel->basic_consume($queueName, "", false, true, false, false, $callabck);
+// 开启消息确认
+$channel->basic_consume($queueName, "", false, false, false, false, $callabck);
+// 运行上面的代码，我们发现即使使用CTRL+C杀掉了一个工作者（worker）进程，消息也不会丢失。当工作者（worker）挂掉这后，所有没有响应的消息都会重新发送。
 
 while (count($channel->callbacks)) {
     $channel->wait();
