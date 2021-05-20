95e5aca7de11e22893a4750a49547d755db7cefe
xiaoxuan6 <1527736751@qq.com>
2021-05-20 10:02:54 +0800

    add mq demo

--- demo/autoload.php
+++ demo/autoload.php
@@ -5,3 +5,9 @@
  * Date: 2021/5/19
  * Time: 15:22
  */
+
+!defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
+
+$config = include BASE_PATH . "/config/autoload/amqp.php";
+
+define("AMQP_CONDIG", $config["default"]);
